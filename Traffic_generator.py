955de327aad3b95bde02d05111776772f46382fd
rooder-saerngi <abhinabbadal11@gmail.com>
2025-12-26 23:11:10 +0545

    Update Traffic_generator.py
    made it so that the if there are more than 5 cars in the special lane they are immediately dequeued and enqueued into other incoming lanes as per the instructions 
    
    also made it so that when the file is imported in simulator it doesn't directly run

--- Traffic_generator.py
+++ Traffic_generator.py
@@ -1,5 +1,5 @@
 import time
-
+import threading
 #Making the class for Queue
 class Queue:
     def __init__(self):
@@ -17,29 +17,14 @@ class Queue:
     def __str__(self):
         return str(self.queue)
 
-class sQueue:
-    def __init__(self):
-        self.queue = []
-    def Senqueue(self, element):
-        self.queue.append(element)
-    def Sdequeue(self):
-        if self.is_empty():
-            return None
-        return self.queue.pop(0)
-    def Sis_empty(self):
-        return len(self.queue) == 0
-    def Ssize(self):
-        return len(self.queue)
-    def __str__(self):
-        return str(self.queue)
 #creating a queue for the lanes
 lanes = {
-    "AL1": Queue(), "AL2": sQueue(), "AL3": Queue(),
-    "BL1": Queue(), "BL2": sQueue(), "BL3": Queue(),
-    "CL1": Queue(), "CL2": sQueue(), "CL3": Queue(),
-    "DL1": Queue(), "DL2": sQueue(), "DL3": Queue(),
+    "AL1": Queue(), "AL2": Queue(), "AL3": Queue(),
+    "BL1": Queue(), "BL2": Queue(), "BL3": Queue(),
+    "CL1": Queue(), "CL2": Queue(), "CL3": Queue(),
+    "DL1": Queue(), "DL2": Queue(), "DL3": Queue(),
 }
-
+moves = Queue()
 #Traffic Lights (every ten seconds the lights change )
 LaneA_lights = "RED"
 LaneB_lights = "RED"
@@ -90,54 +75,118 @@ def generator():
         i += 1
 
         time.sleep(5)
+
+
 def traversal():
-    global LaneA_lights,LaneB_lights,LaneD_lights,LaneC_lights
+    global LaneA_lights, LaneB_lights, LaneC_lights, LaneD_lights
+    global moves
 
-    while True :
-        if LaneA_lights == "GREEN" :
+    while True:
+        al2_size = lanes["AL2"].size()
+        priority_mode = al2_size > 10
 
-            if not lanes["AL2"].is_empty() :
-                car = lanes["AL2"].dequeue()
-                lanes["BL1"].enqueue(car)
-                return"AL2 car moved to BL1"
+        if LaneA_lights == "GREEN":
+            if priority_mode:
+                if not lanes["AL2"].is_empty():
+                    car = lanes["AL2"].dequeue()
+                    lanes["BL1"].enqueue(car)
+                    print("PRIORITY: AL2 car moved to BL1")
+                    moves.enqueue("AL2->BL1")
 
-            elif not lanes["AL3"].is_empty():
-                car = lanes["AL3"].dequeue()
-                lanes["CL1"].enqueue(car)
-                return"AL3 moved to CL1"
+                    if lanes["AL2"].size() < 5:
+                        priority_mode = False
+            else:
+                if not lanes["AL2"].is_empty():
+                    car = lanes["AL2"].dequeue()
+                    lanes["BL1"].enqueue(car)
+                    print("AL2 car moved to BL1")
+                    moves.enqueue("AL2->BL1")
 
-        if LaneB_lights == "GREEN" :
+                if not lanes["AL3"].is_empty():
+                    car = lanes["AL3"].dequeue()
+                    lanes["CL1"].enqueue(car)
+                    print("AL3 car moved to CL1")
+                    moves.enqueue("AL3->CL1")
 
-            if not lanes["BL2"].is_empty() :
-                car = lanes["BL2"].dequeue()
-                lanes["AL1"].enqueue(car)
-                return"BL2 car moved to AL1"
+        if LaneB_lights == "GREEN":
+            if priority_mode and LaneA_lights != "GREEN":
+                if not lanes["AL2"].is_empty():
+                    car = lanes["AL2"].dequeue()
+                    lanes["BL1"].enqueue(car)
+                    print("PRIORITY OVERRIDE: AL2 car moved to BL1 (B green)")
+                    moves.enqueue("AL2->BL1")
 
-            elif not lanes["BL3"].is_empty():
-                car = lanes["BL3"].dequeue()
-                lanes["DL1"].enqueue(car)
-                return"BL3 car moved to DL1"
+                    if lanes["AL2"].size() < 5:
+                        priority_mode = False
+            else:
+                if not lanes["BL2"].is_empty():
+                    car = lanes["BL2"].dequeue()
+                    lanes["CL1"].enqueue(car)
+                    print("BL2 car moved to CL1")
+                    moves.enqueue("BL2->CL1")
 
-        if LaneD_lights == "GREEN" :
+                elif not lanes["BL3"].is_empty():
+                    car = lanes["BL3"].dequeue()
+                    lanes["AL1"].enqueue(car)
+                    print("BL3 car moved to AL1")
+                    moves.enqueue("BL3->AL1")
 
-            if not lanes["DL2"].is_empty() :
-                car = lanes["DL2"].dequeue()
-                lanes["CL1"].enqueue(car)
-                return"DL2 car moved to CL1"
+        if LaneD_lights == "GREEN":
+            if priority_mode and LaneA_lights != "GREEN":
+                if not lanes["AL2"].is_empty():
+                    car = lanes["AL2"].dequeue()
+                    lanes["BL1"].enqueue(car)
+                    print("PRIORITY OVERRIDE: AL2 car moved to BL1 (D green)")
+                    moves.enqueue("AL2->BL1")
 
-            elif not lanes["DL3"].is_empty():
-                car = lanes["DL3"].dequeue()
-                lanes["AL1"].enqueue(car)
-                return"DL3 car moved to AL1"
+                    if lanes["AL2"].size() < 5:
+                        priority_mode = False
+            else:
+                if not lanes["DL2"].is_empty():
+                    car = lanes["DL2"].dequeue()
+                    lanes["CL1"].enqueue(car)
+                    print("DL2 car moved to CL1")
+                    moves.enqueue("DL2->CL1")
 
-        if LaneC_lights == "GREEN" :
+                elif not lanes["DL3"].is_empty():
+                    car = lanes["DL3"].dequeue()
+                    lanes["AL1"].enqueue(car)
+                    print("DL3 car moved to AL1")
+                    moves.enqueue("DL3->AL1")
 
-            if not lanes["CL2"].is_empty() :
-                car = lanes["CL2"].dequeue()
-                lanes["DL1"].enqueue(car)
-                return"CL2 car moved to DL1"
+        if LaneC_lights == "GREEN":
+            if priority_mode and LaneA_lights != "GREEN":
+                if not lanes["AL2"].is_empty():
+                    car = lanes["AL2"].dequeue()
+                    lanes["BL1"].enqueue(car)
+                    print("PRIORITY OVERRIDE: AL2 car moved to BL1 (C green)")
+                    moves.enqueue("AL2->BL1")
 
-            elif not lanes["CL3"].is_empty():
-                car = lanes["CL3"].dequeue()
-                lanes["BL1"].enqueue(car)
-                return"CL3 car moved to BL1"
+                    if lanes["AL2"].size() < 5:
+                        priority_mode = False
+            else:
+                if not lanes["CL2"].is_empty():
+                    car = lanes["CL2"].dequeue()
+                    lanes["DL1"].enqueue(car)
+                    print("CL2 car moved to DL1")
+                    moves.enqueue("CL2->DL1")
+
+                elif not lanes["CL3"].is_empty():
+                    car = lanes["CL3"].dequeue()
+                    lanes["BL1"].enqueue(car)
+                    print("CL3 car moved to BL1")
+                    moves.enqueue("CL3->BL1")
+
+        time.sleep(1)
+if __name__ == "__main__":
+    print("Running Traffic Generator directly")
+    threading.Thread(target=lights_changer, daemon=True).start()
+    threading.Thread(target=generator, daemon=True).start()
+    threading.Thread(target=traversal, daemon=True).start()
+
+    # Keep main thread alive
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Stopped")
